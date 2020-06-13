c7a14a9a6528914d9429d49ecbe9e44b608e7960
Андрей <anastasiiapegar@gmail.com>
2020-06-13 14:30:40 +0300

    29(we add state.tsx with all data) lesson Social network

---
+++ my-app/src/Redux/State.tsx
@@ -0,0 +1,30 @@
+
+let state = {
+    postsData:[{id:1, message: 'How are you?', likes: '12'},
+        {id:2, message: 'My name is Andrew', likes: '7'},
+        {id:3, message: 'How are you?', likes: '12'},
+        {id:4, message: 'How are you?', likes: '12'},
+        {id:5, message: 'My name is Andrew', likes: '777'}],
+    dialogsData:[
+        {id:1, name: 'Dima'},
+        {id:2, name: 'Andrew'},
+        {id:3, name: 'Kostya'},
+        {id:4, name: 'Lexa'}
+
+    ],
+    messagesData:[
+        {id:1, text: 'Hello'},
+        {id:2, text: 'Hello'},
+        {id:3, text: 'Petya'},
+        {id:4, text: 'Goodbye'}
+    ]
+    }
+ export default state;
+
+
+
+
+
+
+
+
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import state from "./Redux/State";
 
 export type indexT = {
     id: number,
@@ -22,33 +22,33 @@ export type indexTTT = {
 
 
 
-let postsData = [{id:1, message: 'How are you?', likes: '12'},
-    {id:2, message: 'My name is Andrew', likes: '7'},
-    {id:3, message: 'How are you?', likes: '12'},
-    {id:4, message: 'How are you?', likes: '12'},
-    {id:5, message: 'My name is Andrew', likes: '777'}
-];
-
-let dialogsData = [
-    {id:1, name: 'Dima'},
-    {id:2, name: 'Andrew'},
-    {id:3, name: 'Kostya'},
-    {id:4, name: 'Lexa'}
-
-];
-let messagesData = [
-    {id:1, text: 'Hello'},
-    {id:2, text: 'Hello'},
-    {id:3, text: 'Petya'},
-    {id:4, text: 'Goodbye'}
-];
+// let postsData = [{id:1, message: 'How are you?', likes: '12'},
+//     {id:2, message: 'My name is Andrew', likes: '7'},
+//     {id:3, message: 'How are you?', likes: '12'},
+//     {id:4, message: 'How are you?', likes: '12'},
+//     {id:5, message: 'My name is Andrew', likes: '777'}
+// ];
+//
+// let dialogsData = [
+//     {id:1, name: 'Dima'},
+//     {id:2, name: 'Andrew'},
+//     {id:3, name: 'Kostya'},
+//     {id:4, name: 'Lexa'}
+//
+// ];
+// let messagesData = [
+//     {id:1, text: 'Hello'},
+//     {id:2, text: 'Hello'},
+//     {id:3, text: 'Petya'},
+//     {id:4, text: 'Goodbye'}
+// ];
 
 ReactDOM.render(
     <React.StrictMode>
         <App
-            postsData={postsData}
-            dialogsData={dialogsData}
-            messagesData={messagesData}
+            postsData={state.postsData}
+            dialogsData={state.dialogsData}
+            messagesData={state.messagesData}
 
         />
     </React.StrictMode>,
