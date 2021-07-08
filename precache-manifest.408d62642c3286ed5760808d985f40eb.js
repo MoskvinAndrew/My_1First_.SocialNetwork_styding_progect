1f83775b4ac88fe4b65eef41441d6eab05e2f817
Андрей <anastasiiapegar@gmail.com>
2021-07-08 22:59:52 +0300

    Updates

---
+++ precache-manifest.408d62642c3286ed5760808d985f40eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3033bbd84be7de1b7415efe0b581fadc",
+    "url": "/My_1First_.SocialNetwork_styding_progect/index.html"
+  },
+  {
+    "revision": "4ad0092a9ac8ac09676d",
+    "url": "/My_1First_.SocialNetwork_styding_progect/static/css/2.67e5f240.chunk.css"
+  },
+  {
+    "revision": "fa44d3daecb6733153c4",
+    "url": "/My_1First_.SocialNetwork_styding_progect/static/css/main.a04b8fb6.chunk.css"
+  },
+  {
+    "revision": "4ad0092a9ac8ac09676d",
+    "url": "/My_1First_.SocialNetwork_styding_progect/static/js/2.62d8fc19.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/My_1First_.SocialNetwork_styding_progect/static/js/2.62d8fc19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa44d3daecb6733153c4",
+    "url": "/My_1First_.SocialNetwork_styding_progect/static/js/main.92b66292.chunk.js"
+  },
+  {
+    "revision": "2d1d3adfeca2b6cc00c4",
+    "url": "/My_1First_.SocialNetwork_styding_progect/static/js/runtime-main.f0417502.js"
+  },
+  {
+    "revision": "ad84532ba54f5d9e2f54bd9ed4db6f23",
+    "url": "/My_1First_.SocialNetwork_styding_progect/static/media/light-aircraft-pilot-course-spain.ad84532b.jpg"
+  },
+  {
+    "revision": "a5db8300587115c0ec629ac982af8530",
+    "url": "/My_1First_.SocialNetwork_styding_progect/static/media/unnamed.a5db8300.gif"
+  }
+]);
